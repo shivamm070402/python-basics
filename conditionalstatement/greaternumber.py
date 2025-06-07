796a3a01b5eee6202be1d9a4f152e08a1650a723
shivamm070402 <shivammohite2020@gmail.com>
2025-06-07 10:04:46 +0530

    program

--- conditionalstatement/greaternumber.py
+++ conditionalstatement/greaternumber.py
@@ -1,3 +1,5 @@
+
+# This code checks if a number is greater than 15 and prints a message if true.
 i = 10
 
  # Checking if i is greater than 15
@@ -5,3 +7,8 @@ if (i > 15):
     print("10 is less than 15")
     
 print("I am Not in if")
+
+# This code checks if a number is greater than 18 and prints a message if true.
+
+age = 19
+if age > 18: print("Eligible to Vote.")
