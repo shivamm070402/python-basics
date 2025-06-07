16f9bd85851aa2b093afcf86b4f569436fa858bf
shivamm070402 <shivammohite2020@gmail.com>
2025-06-07 08:45:05 +0530

    first day

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
+# This is a simple Python script that prints "Hello, World!" to the console.
 print("Hello, World!")
 
+# The following line defines a string variable and prints it.
 my_string = "Hello, World!"
 print(my_string)
